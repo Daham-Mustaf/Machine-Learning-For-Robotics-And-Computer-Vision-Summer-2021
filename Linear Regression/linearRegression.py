be532ff5dc4dc07008a465c61aecf60d126deff9
Daham-Mustaf <s7damust@uni-bonn.de>
2023-04-23 19:23:27 +0200

    LR

--- Linear Regression/linearRegression.py
+++ Linear Regression/linearRegression.py
@@ -1,95 +1,75 @@
-
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn import datasets
-import pandas as pd
-from matplotlib import pyplot as plt
 
-import numpy as np
+
+def r2_score(y_true, y_pred):
+    corr_matrix = np.corrcoef(y_true, y_pred)
+    corr = corr_matrix[0, 1]
+    return corr ** 2
 
 
 class LinearRegression:
-
-    def __init__(self, lr = 0.001, n_iters=1000):
-        self.lr = lr
+    def __init__(self, learning_rate=0.001, n_iters=1000):
+        self.lr = learning_rate
         self.n_iters = n_iters
         self.weights = None
         self.bias = None
 
     def fit(self, X, y):
-        # if X.ndim == 1:
-        #     X = X.reshape(-1, 1)
-
         n_samples, n_features = X.shape
-        self.weights = np.zeros(n_samples)
+
+        # init parameters
+        self.weights = np.zeros(n_features)
         self.bias = 0
 
+        # gradient descent
         for _ in range(self.n_iters):
-            y_pred = np.dot(X, self.weights) + self.bias
+            y_predicted = np.dot(X, self.weights) + self.bias
+            # compute gradients
+            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
+            db = (1 / n_samples) * np.sum(y_predicted - y)
 
-            dw = (1/n_samples) * np.dot(X.T, (y_pred-y))
-            db = (1/n_samples) * np.sum(y_pred-y)
-
-            self.weights = self.weights - self.lr * dw
-            self.bias = self.bias - self.lr * db
-
+            # update parameters
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
 
     def predict(self, X):
-        y_pred = np.dot(X, self.weights.reshape(-1, 1)) + self.bias
-        return y_pred
-
-df = pd.read_csv('linear.csv')
-X = df[['X']].values
-y = df['Y'].values
-X_train, X_test, y_train, y_test = train_test_split(df['X'], df['Y'], test_size=0.2)
-from sklearn.linear_model import LinearRegression
-clf = LinearRegression()
-clf.fit(X_train, y_train)
+        y_approximated = np.dot(X, self.weights) + self.bias
+        return y_approximated
 
 
-import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
+# Testing
+if __name__ == "__main__":
+    # Imports
+    import matplotlib.pyplot as plt
+    from sklearn.model_selection import train_test_split
+    from sklearn import datasets
 
-df = pd.read_csv('linear.csv')
-X = df[['X']]  # Use double brackets to create a 2D array
-y = df['Y']
+    def mean_squared_error(y_true, y_pred):
+        return np.mean((y_true - y_pred) ** 2)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X, y = datasets.make_regression(
+        n_samples=100, n_features=1, noise=20, random_state=4
+    )
 
-clf = LinearRegression()
-clf.fit(X_train, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=1234
+    )
 
-clf.score(X_test, y_test)
+    regressor = LinearRegression(learning_rate=0.01, n_iters=1000)
+    regressor.fit(X_train, y_train)
+    predictions = regressor.predict(X_test)
 
+    mse = mean_squared_error(y_test, predictions)
+    print("MSE:", mse)
 
+    accu = r2_score(y_test, predictions)
+    print("Accuracy:", accu)
 
-
-
-
-
-
-# plt.scatter(X, y)
-# plt.xlabel("X")
-# plt.ylabel("Y")
-# plt.show()
-X = X.reshape(-1, 1)
-
-n_samples, n_features = X.shape
-weights = np.zeros(n_samples)
-
-fig = plt.figure(figsize=(8,6))
-plt.scatter(X, y, color = "b", marker = "o", s = 30)
-plt.show()
-
-
-reg = LinearRegression(lr=0.01)
-reg.fit(X_train,y_train)
-X_test.shape
-predictions = reg.predict(X_test)
-
-def mse(y_test, predictions):
-    return np.mean((y_test-predictions)**2)
-
-mse = mse(y_test, predictions)
-print(mse)
+    y_pred_line = regressor.predict(X)
+    cmap = plt.get_cmap("viridis")
+    fig = plt.figure(figsize=(8, 6))
+    m1 = plt.scatter(X_train, y_train, color=cmap(0.9), s=10)
+    m2 = plt.scatter(X_test, y_test, color=cmap(0.5), s=10)
+    plt.plot(X, y_pred_line, color="black", linewidth=2, label="Prediction")
+    plt.show()
+    
